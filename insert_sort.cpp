6548481c65bbad27fd9d0c353df4132d09440367
geekzzz <820760596@qq.com>
2017-11-06 22:08:23 +0800

    change insert_sort

--- insert_sort.cpp
+++ insert_sort.cpp
@@ -11,10 +11,10 @@
 
 using namespace std;
 
-void insertSort(int arr[], int length)
+void insertSort(vector<int> arr)
 {
     int j, key;
-    for (int i = 1; i < length; i++){
+    for (int i = 1; i < arr.size(); i++){
         key = arr[i];
         j = i - 1;
         while (j >= 0 && arr[j] > key){
@@ -23,14 +23,15 @@ void insertSort(int arr[], int length)
         }
         arr[j + 1] = key;
     }
-    for (int i = 0;i<6;i++)
+    for (int i = 0;i<arr.size();i++)
     {
         cout << arr[i];
     }
 }
 
 int main(int argc, const char * argv[]) {
-    int zz[] ={5,2,4,6,1,3};
-    insertSort(zz, 6);
+    // insert code here...
+    vector<int> zz ={5,2,4,6,1,3};
+    insertSort(zz);
     return 0;
 }
