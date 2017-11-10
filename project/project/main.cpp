c347cf7e1287501c02006340103932ad6188eb2d
geekzzz <820760596@qq.com>
2017-11-10 19:27:06 +0800

    renew heap_sort.cpp

--- project/project/main.cpp
+++ project/project/main.cpp
@@ -54,11 +54,14 @@ void heap(int a[],int heap_size)
 int main(int argc, const char * argv[]) {
     // insert code here...
     int heap_size = 10;        //堆元素存储在数组中的个数
-    int a[heap_size+1];
+    int a[] {0,12,42,52,11,3,5,6,98,100,11};
+    /*
     for(auto i = 1;i <= heap_size;++i)
     {
-        a[i] = i;
+        a[i] = heap_size-i;
     }
+     */
+    
     cout << "zzz"<< endl;
     for(auto j = 1;j<=heap_size;++j)
     {
@@ -68,7 +71,14 @@ int main(int argc, const char * argv[]) {
     build_max_heap(a, heap_size);
     //heap(a,heap_size);
     
-    cout << "zzz"<< endl;
+    cout << "堆内数据顺序"<< endl;
+    for(auto j = 1;j<=heap_size;++j)
+    {
+        cout << a[j] << ":";
+    }
+    cout << endl;
+    heap(a, heap_size);
+    cout << "排序完成"<< endl;
     for(auto j = 1;j<=heap_size;++j)
     {
         cout << a[j] << ":";
