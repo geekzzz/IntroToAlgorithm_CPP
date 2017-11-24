0d31e0d819cd497e8bdf7a627598eed7c9b47700
geekzzz <820760596@qq.com>
2017-11-24 23:14:42 +0800

    增加简单的双向链表

--- project/project/main.cpp
+++ project/project/main.cpp
@@ -1,4 +1,4 @@
-//根据CLRS中算法编写,简单的数组实现队列
+//双向链表的简单实现，根据clrs
 //
 #include <iostream>
 #include <vector>
@@ -8,89 +8,109 @@
 #include <ctime>
 #include <stdlib.h>
 using namespace std;
+struct Head;
+struct Double_list;
+struct Node;
 
-#define queue_max_size 1000
-
-class queue
+class L
 {
 public:
-    bool queue_empty();
-    bool enqueues(int x);
-    int dequeue();
-    int head = 0;  //head指向队列头
-    int tail = 0;  //tail指向队尾
-    int queue[queue_max_size];
+    L(Node *head = NULL) : head(head) {}
+    
+    void list_insert(Node *x);
+    Node *list_search(int k);
+    void list_delete(Node *x);
+    
+    Node  *head = NULL;
 };
 
-bool queue::queue_empty()
+class Node
 {
-    if(head == tail)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool queue::enqueues(int x)
-{
-    if(head == tail +1)
-    {
-        cout << "overflow  上溢出" << endl;
-        return false;
-    }
-    else
-    {
-        queue[tail] = x;
-        if(tail == queue_max_size - 1)
-        {
-            tail = 0;
-        }
-        else
-        {
-            tail += 1;
-        }
-        return true;
-    }
-}
-
-int queue::dequeue()
-{
-    if(queue_empty())
-    {
-        cout << "overflow 下溢出" << endl;
-        return 0;
-    }
-    else
-    {
-        int x = queue[head];
-        if(head == queue_max_size - 1)
-        {
-            head = 0;
-        }
-        else
-        {
-            head += 1;
-        }
-        return x;
-    }
+public:
+    friend class L;
+    Node(int n = 0,Node *pre = NULL,Node *next = NULL) : n(n),pre(pre),next(next){}
     
+    Node *pre ,*next;
+    int n ;
+    
+};
+
+Node *L::list_search(int k)
+{
+    Node *x = NULL;
+    x = head;
+    while(x != NULL && x->n != k)
+    {
+        x = x->next;
+    }
+    return x;
+}
+
+
+void L::list_insert(Node *x)
+{
+    x->next = head;
+    if(head != NULL)
+    {
+        head->pre = x;
+    }
+    head = x;
+}
+
+void L::list_delete(Node *x)
+{
+    if(x->pre != NULL)
+    {
+        x->pre->next = x->next;
+    }
+    else
+    {
+        head = x->next;
+    }
+    if(x->next != NULL)
+    {
+        x->next->pre = x->pre;
+    }
 }
 
 int  main(int argc,const char *argv[])
 {
-    queue q;
-    bool e = q.queue_empty();
-    cout << e << endl;
-    q.enqueues(100);
-    e= q.queue_empty();
-    q.enqueues(1000);
-    int x = q.dequeue();
-    cout << q.queue_empty() << endl;
-    cout << x << endl;
-    x = q.dequeue();
-    cout << x << endl;
-    cout <<q.queue_empty() << endl;
+    L *list  = new L();
+    for(int i = 0;i< 10;i++)
+    {
+        Node *p = new Node();
+        p->n = i;
+        list->list_insert(p);
+    }
+    /*
+    Node *p = list->head->next;
+    for(int i = 0;i < 10;i++)
+    {
+        
+        if(p != NULL)
+        {
+            cout << p->n << endl;
+            p = p->next;
+        }
+    }
+     */
+    cout << endl;
+    for(int i = 0;i< 10 ;i++)
+    {
+        Node *m = list->list_search(i);
+        cout << m->n << endl;
+    }
+    
+    list->list_delete(list->list_search(5));
+    list->list_delete(list->list_search(8));
+    cout << endl;
+    for(int i = 0;i< 10 ;i++)
+    {
+        Node *m = list->list_search(i);
+        if( m != NULL)
+        {
+            cout << m->n << endl;
+        }
+        
+    }
 }
