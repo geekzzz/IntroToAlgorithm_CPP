842e7a1a848dc50330a227c74d987c1f9c7a37a2
geekzzz <820760596@qq.com>
2018-08-01 09:47:55 +0800

    18.8.1 lc

--- LeetCodePractice/easy/array/single number.cpp
+++ LeetCodePractice/easy/array/single number.cpp
@@ -17,3 +17,17 @@ public:
 		return m.begin()->first;
 	}
 };
+
+
+//第二种解法
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		int ans = 0;
+		for (int i = 0; i < nums.size(); ++i)
+		{
+			ans ^= nums[i];
+		}
+		return ans;
+	}
+};
