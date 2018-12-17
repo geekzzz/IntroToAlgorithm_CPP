5183b2f92a38fcd80eec3e49b26437af35f23650
geekzzz <820760596@qq.com>
2018-12-17 10:51:49 +0800

    add c++

---
+++ c++/纯虚函数.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <set>
+#include <string>
+using namespace std;
+
+class Shape
+{
+public:
+	// 提供接口框架的纯虚函数
+	virtual int getArea() = 0;
+	void setWidth(int w)
+	{
+		width = w;
+	}
+	void setHeight(int h)
+	{
+		height = h;
+	}
+protected:
+	int width;
+	int height;
+};
+
+// 派生类
+class Rectangle : public Shape
+{
+public:
+	int getArea()
+	{
+		return (width * height);
+	}
+};
+class Triangle : public Shape
+{
+public:
+	int getArea()
+	{
+		return (width * height) / 2;
+	}
+};
+
+int main()
+{
+	Rectangle Rect;
+	Triangle  Tri;
+
+	Rect.setWidth(5);
+	Rect.setHeight(7);
+	// 输出对象的面积
+	cout << "Total Rectangle area: " << Rect.getArea() << endl;
+
+	Tri.setWidth(5);
+	Tri.setHeight(7);
+	// 输出对象的面积
+	cout << "Total Triangle area: " << Tri.getArea() << endl;
+
+	system("pause");
+}
