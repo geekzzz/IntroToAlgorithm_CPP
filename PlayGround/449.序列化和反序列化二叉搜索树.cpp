cde65a8c17b8307c9836c4f0f8fe07c8d543c95c
hitonzhang <hitonzhang@tencent.com>
2021-05-10 10:39:53 +0800

    add lc

--- PlayGround/449.序列化和反序列化二叉搜索树.cpp
+++ PlayGround/449.序列化和反序列化二叉搜索树.cpp
@@ -28,17 +28,37 @@ public:
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        
+        list<string> s = split(data,",");
+        TreeNode * tree = build(s);
+        return tree;
     }
 
-    vector<string> split(strign s, string c)
+    TreeNode* build(list<string>& s)
     {
+        string z = s.front();
+        s.pop_front();
+        if(z == "X")
+            return nullptr;
+        
+        TreeNode* root = new TreeNode();
+        root->val = stoi(z);
+        root->left = build(s);
+        root->right = build(s);
+        return root;
+    }
+
+    list<string> split(string s, string c)
+    {
+        list<string> res;
         int lastpos = s.find_first_not_of(c,0);
         int pos = s.find_first_of(c,lastpos);
         while(lastpos != string::npos || pos != string::npos)
         {
-            
+            res.push_back(s.substr(lastpos,pos - lastpos));
+            lastpos = s.find_first_not_of(c,pos);
+            pos = s.find_first_of(c,lastpos);
         }
+        return res;
     }
 };
 
